9a307b9216316595f1cafb761a84c818ae970de3
Joel M. Boada <jmboada30@gmail.com>
2022-12-13 00:06:59 -0400

    add tanstack

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+
 import App from './App.vue';
 
 import './assets/main.css';
 import router from './router';
 
 const app = createApp(App);
+
+app.use(VueQueryPlugin);
 app.use(router);
 app.mount('#app');
