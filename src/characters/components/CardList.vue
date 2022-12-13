9ac121b18315dbc1a2584d8a367463a4608c6bf6
Joel M. Boada <jmboada30@gmail.com>
2022-12-13 00:18:55 -0400

    usando useQuery

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-import { useCharacters } from '@/characters/composables/useCharacters';
-const { isLoading, characters, hasError, errorMessage } = useCharacters();
+// USANDO COMPOSABLES
+// import { useCharacters } from '@/characters/composables/useCharacters';
+// const { isLoading, characters, hasError, errorMessage } = useCharacters();
+
+// USANDO @tanstack/vue-query
+import { useQuery } from '@tanstack/vue-query';
+
+const getCharacterSlow = async () => {
+  const response = await fetch('https://rickandmortyapi.com/api/character');
+  const data = await response.json();
+  return data.results;
+};
+
+const {
+  data: characters,
+  isError,
+  error,
+  isLoading,
+} = useQuery(['characters'], getCharacterSlow);
 </script>
 
 <template>
   <h1 v-if="isLoading">Loading...</h1>
-  <h1 v-if="hasError">{{ errorMessage }}</h1>
+  <h1 v-if="isError">{{ error }}</h1>
   <ul>
     <li v-for="character of characters" :key="character.id">
       {{ character.name }}
