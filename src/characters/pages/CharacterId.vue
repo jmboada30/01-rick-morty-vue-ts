b3ec16bd56d18559d7980aee4bc34c78b90a71ce
Joel M. Boada <jmboada30@gmail.com>
2022-12-14 06:45:34 -0400

    add getCharacterCacheFirst in page CharacterId

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import rickAndMortyApi from "@/api/rickAndMortyApi";
+import characterStore from "@/store/characters.store";
+import { useQuery } from "@tanstack/vue-query";
+import { useRoute } from "vue-router";
+import type { CharacterRickMorty } from "../interfaces/character";
 
 const route = useRoute();
+const { id } = route.params as { id: string };
+
+const getCharacterCacheFirst = async (characterId: string) => {
+  if (characterStore.checkIdInStore(characterId)) {
+    return characterStore.ids.list[characterId];
+  }
+
+  const { data } = await rickAndMortyApi.get<CharacterRickMorty>(
+    `/character/${characterId}`
+  );
+  return data;
+};
+
+const { data } = useQuery(["characters", id], () => getCharacterCacheFirst(id), {
+  onSuccess: (character) => {
+    characterStore.loadedCharacter(character);
+  },
+});
 </script>
 
 <template>
   <div>
-    <h1>Character # {{route.params.id}}</h1>
+    <h1>Character # {{ id }}</h1>
+
+    <code>
+      {{ data }}
+    </code>
   </div>
 </template>
-
 
 <style scoped></style>
